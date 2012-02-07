ae9d222f2c3cffc0d8b9aa1a9e78d7f012c6b5ac
Richard Nguyen <rich.nguy@gmail.com>
2012-02-06 18:59:39 -0800

    Improvements to the unit test system with simple examples.

--- Platform/UnitTest/TestFixture.lua
+++ Platform/UnitTest/TestFixture.lua
@@ -6,13 +6,20 @@ function TestFixture:New()
 	local instance = {}
 	instance.testCases = {}
 	
-	function instance:AssertEquals( expected, actual, description )
-		if expected ~= actual then
-			error( description or "" )
-		end
+	function instance:AssertEquals( expected, actual )
+		local result = expected == actual
+		local message = result and "" or string.format( "%s does not equal %s", tostring(actual), tostring(expected) )
+		return result, message
+	end
+
+	function instance:AssertTrue( test )
+		local result = test == true
+		local message = result and "" or string.format( "Expected true, but got false" )
+		return result, message
 	end
 	
 	function instance:Run()
+		local results = {}
 		if instance.SetUp then
 			instance:SetUp()
 		end
@@ -20,13 +27,22 @@ function TestFixture:New()
 		for _, testName in ipairs( self.testCases ) do
 			local testCase = self[testName]
 			if type( testCase ) == "function" then
-				testCase(instance)
+				local testResult, testMessage = testCase(instance)
+				local testInfo = {
+					name = testName,
+					result = testResult,
+					message = testMessage
+				}
+
+				table.insert( results, testInfo )
 			end
 		end
 
 		if instance.TearDown then
 			instance:TearDown()
 		end
+
+		return results
 	end
 	
 	local mt = {}
--- Platform/UnitTest/UtilTestFixture.lua
+++ Platform/UnitTest/UtilTestFixture.lua
@@ -7,19 +7,41 @@ UtilTestFixture = TestFixture:New()
 function UtilTestFixture:SetUp()
 end
 
-function UtilTestFixture:TestTableToString()
-	local result = true
-	self:AssertEquals( "{", ToString( {} ) )
-
-	return result
+function UtilTestFixture:TestTableToString1()
+	return self:AssertEquals( "{}", ToString( {} ) )
 end
 
-function UtilTestFixture:TestPointRectIntersect()
-	print( "test 3" )
+function UtilTestFixture:TestTableToString2()
+	local expected = [[{
+  ["a"]=1,
+  ["c"]=3,
+  ["b"]=2,
+}]]
+	return self:AssertEquals( expected, ToString( { a = 1, b = 2, c = 3 } ) )
+end
+
+function UtilTestFixture:TestTableToString3()
+	local expected = [[{
+  ["a"]=1,
+  ["b"]={
+    ["c"]=3,
+  },
+}]]
+	return self:AssertEquals( expected, ToString( { a = 1, b = { c = 3} } ) )
+end
+
+function UtilTestFixture:TestPointRectIntersect1()
+	local point_x, point_y = 0, 0
+	local rect_x1, rect_y1 = 0, 0
+	local rect_x2, rect_y2 = 0, 0
+
+	return self:AssertTrue( PointRectIntersect( point_x, point_y, rect_x1, rect_y1, rect_x2, rect_y2 ) )
 end
 
 function UtilTestFixture:TearDown()
-	print( "test 4" )
 end
 
-UtilTestFixture:Run()
+local results = UtilTestFixture:Run()
+for _, fixtureResults in ipairs( results ) do
+	print( fixtureResults.name, fixtureResults.result and "passed" or "failed", fixtureResults.message )
+end
